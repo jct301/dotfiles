261f504fd0e64d557f3ce5f94e5e417af8323d97
jc301 <jc301@gmail.com>
2024-01-23 20:53:33 -0400

    add neovim config initial

---
+++ .config/nvim/init.lua
@@ -0,0 +1,10 @@
+local cmd = vim.cmd
+local exec = vim.api.nvim_exec
+local fn = vim.fn
+local g = vim.g
+local opt = vim.opt
+local api = vim.api
+local ag = vim.api.nvim_create_augroup
+local au = vim.api.nvim_create_autocmd
+
+
